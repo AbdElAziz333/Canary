b0c45a57acf2062605b320517f2ae55bb7d2c93d
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-09-16 12:49:50 +0200

    remove: 1.19.2 optimizations

--- src/main/java/com/abdelaziz/canary/common/block/entity/inventory_comparator_tracking/ComparatorTracker.java
+++
@@ -1,9 +0,0 @@
-package com.abdelaziz.canary.common.block.entity.inventory_comparator_tracking;
-
-import net.minecraft.util.math.Direction;
-
-public interface ComparatorTracker {
-    void onComparatorAdded(Direction direction, int offset);
-
-    boolean hasAnyComparatorNearby();
-}
