f2db330d9c6000acb2d854f29bb2ad47210d086d
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-09-23 02:15:44 +0200

    fix: hoppers not tracking inventory above when they are not full.

--- src/main/java/com/abdelaziz/canary/mixin/block/hopper/HopperBlockEntityMixin.java
+++ src/main/java/com/abdelaziz/canary/mixin/block/hopper/HopperBlockEntityMixin.java
@@ -713,7 +713,7 @@ public abstract class HopperBlockEntityMixin extends BlockEntity implements Hopp
 
                 CanaryStackList thisStackList = InventoryHelper.getCanaryStackList(this);
 
-                if (this.extractionMode != HopperCachingState.BlockInventory.BLOCK_STATE && thisStackList.getFullSlots() == thisStackList.size()) {
+                if (this.extractionMode != HopperCachingState.BlockInventory.BLOCK_STATE && thisStackList.getFullSlots() != thisStackList.size()) {
                     if (this.extractionMode == HopperCachingState.BlockInventory.REMOVAL_TRACKING_BLOCK_ENTITY) {
                         Inventory blockInventory = this.extractBlockInventory;
                         if (this.extractStackList != null &&
