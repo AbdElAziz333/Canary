0dfde9e195aea554f7ff873d162df5b4d1c1d124
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-10-22 03:01:07 +0200

    fix: use set instead of list to avoid duplicated entries and memory leak with sleeping hoppers

--- src/main/java/com/abdelaziz/canary/common/entity/tracker/nearby/SectionedEntityMovementTracker.java
+++ src/main/java/com/abdelaziz/canary/common/entity/tracker/nearby/SectionedEntityMovementTracker.java
@@ -6,6 +6,7 @@ import com.abdelaziz.canary.mixin.ai.nearby_entity_tracking.ServerWorldAccessor;
 import it.unimi.dsi.fastutil.HashCommon;
 import com.abdelaziz.canary.common.entity.tracker.EntityTrackerEngine;
 import com.abdelaziz.canary.common.entity.tracker.EntityTrackerSection;
+import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.world.entity.EntityLike;
@@ -25,7 +26,7 @@ public abstract class SectionedEntityMovementTracker<E extends EntityLike, S> {
 
     private long maxChangeTime;
 
-    private ArrayList<NearbyEntityMovementListener> nearbyEntityMovementListeners;
+    private ReferenceOpenHashSet<NearbyEntityMovementListener> nearbyEntityMovementListeners;
 
     public SectionedEntityMovementTracker(WorldSectionBox interactionChunks, Class<S> clazz) {
         this.clazz = clazz;
@@ -166,7 +167,7 @@ public abstract class SectionedEntityMovementTracker<E extends EntityLike, S> {
 
     public void listenToEntityMovementOnce(NearbyEntityMovementListener listener) {
         if (this.nearbyEntityMovementListeners == null) {
-            this.nearbyEntityMovementListeners = new ArrayList<>();
+            this.nearbyEntityMovementListeners = new ReferenceOpenHashSet<>();
         }
         this.nearbyEntityMovementListeners.add(listener);
 
@@ -178,12 +179,12 @@ public abstract class SectionedEntityMovementTracker<E extends EntityLike, S> {
 
     public void emitEntityMovement(int classMask, EntityTrackerSection section) {
         if ((classMask & (1 << this.trackedClass)) != 0) {
-            ArrayList<NearbyEntityMovementListener> listeners = this.nearbyEntityMovementListeners;
+            ReferenceOpenHashSet<NearbyEntityMovementListener> listeners = this.nearbyEntityMovementListeners;
             if (listeners != null) {
-                for (int i = listeners.size() - 1; i >= 0; i--) {
-                    NearbyEntityMovementListener listener = listeners.remove(i);
+                for (NearbyEntityMovementListener listener : listeners) {
                     listener.handleEntityMovement(this.clazz);
                 }
+                listeners.clear();
             }
             this.sectionsNotListeningTo.add(section);
         }
