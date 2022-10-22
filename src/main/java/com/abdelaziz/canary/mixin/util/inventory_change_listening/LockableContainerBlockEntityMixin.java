a50c6a7136923599b5d7a3164196004db5ad3869
AbdElAziz333 <89092953+AbdElAziz333@users.noreply.github.com>
2022-10-22 02:36:02 +0200

    fix: use set instead of using list to fix: use set instead of list to avoid duplicated entries and memory leak with sleeping hoppers

--- src/main/java/com/abdelaziz/canary/mixin/util/inventory_change_listening/LockableContainerBlockEntityMixin.java
+++ src/main/java/com/abdelaziz/canary/mixin/util/inventory_change_listening/LockableContainerBlockEntityMixin.java
@@ -5,6 +5,7 @@ import com.abdelaziz.canary.common.block.entity.inventory_change_tracking.Invent
 import com.abdelaziz.canary.common.block.entity.inventory_change_tracking.InventoryChangeListener;
 import com.abdelaziz.canary.common.block.entity.inventory_change_tracking.InventoryChangeTracker;
 import com.abdelaziz.canary.common.hopper.CanaryStackList;
+import it.unimi.dsi.fastutil.objects.ReferenceArraySet;
 import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
 import net.minecraft.block.entity.LockableContainerBlockEntity;
 import net.minecraft.inventory.Inventory;
@@ -14,23 +15,23 @@ import java.util.ArrayList;
 
 @Mixin(LockableContainerBlockEntity.class)
 public abstract class LockableContainerBlockEntityMixin implements InventoryChangeEmitter, Inventory {
-    ArrayList<InventoryChangeListener> inventoryChangeListeners = null;
-    ReferenceOpenHashSet<InventoryChangeListener> inventoryHandlingTypeListeners = null;
+    ReferenceArraySet<InventoryChangeListener> inventoryChangeListeners = null;
+    ReferenceArraySet<InventoryChangeListener> inventoryHandlingTypeListeners = null;
 
     @Override
     public void emitContentModified() {
-        ArrayList<InventoryChangeListener> inventoryChangeListeners = this.inventoryChangeListeners;
+        ReferenceArraySet<InventoryChangeListener> inventoryChangeListeners = this.inventoryChangeListeners;
         if (inventoryChangeListeners != null) {
-            for (int i = inventoryChangeListeners.size() - 1; i >= 0; i--) {
-                InventoryChangeListener inventoryChangeListener = inventoryChangeListeners.remove(i);
+            for (InventoryChangeListener inventoryChangeListener : inventoryChangeListeners) {
                 inventoryChangeListener.handleInventoryContentModified(this);
             }
+            inventoryChangeListeners.clear();
         }
     }
 
     @Override
     public void emitStackListReplaced() {
-        ReferenceOpenHashSet<InventoryChangeListener> listeners = this.inventoryHandlingTypeListeners;
+        ReferenceArraySet<InventoryChangeListener> listeners = this.inventoryHandlingTypeListeners;
         if (listeners != null && !listeners.isEmpty()) {
             listeners.forEach(inventoryChangeListener -> inventoryChangeListener.handleStackListReplaced(this));
         }
@@ -42,7 +43,7 @@ public abstract class LockableContainerBlockEntityMixin implements InventoryChan
 
     @Override
     public void emitRemoved() {
-        ReferenceOpenHashSet<InventoryChangeListener> listeners = this.inventoryHandlingTypeListeners;
+        ReferenceArraySet<InventoryChangeListener> listeners = this.inventoryHandlingTypeListeners;
         if (listeners != null && !listeners.isEmpty()) {
             listeners.forEach(listener -> listener.handleInventoryRemoved(this));
         }
@@ -54,19 +55,19 @@ public abstract class LockableContainerBlockEntityMixin implements InventoryChan
 
     @Override
     public void emitFirstComparatorAdded() {
-        ArrayList<InventoryChangeListener> inventoryChangeListeners = this.inventoryChangeListeners;
+        ReferenceArraySet<InventoryChangeListener> inventoryChangeListeners = this.inventoryChangeListeners;
         if (inventoryChangeListeners != null) {
-            for (int i = inventoryChangeListeners.size() - 1; i >= 0; i--) {
-                InventoryChangeListener inventoryChangeListener = inventoryChangeListeners.remove(i);
+            for (InventoryChangeListener inventoryChangeListener : inventoryChangeListeners) {
                 inventoryChangeListener.handleComparatorAdded(this);
             }
+            inventoryChangeListeners.clear();
         }
     }
 
     @Override
     public void forwardContentChangeOnce(InventoryChangeListener inventoryChangeListener, CanaryStackList stackList, InventoryChangeTracker thisTracker) {
         if (this.inventoryChangeListeners == null) {
-            this.inventoryChangeListeners = new ArrayList<>(1);
+            this.inventoryChangeListeners = new ReferenceArraySet<>(1);
         }
         stackList.setInventoryModificationCallback(thisTracker);
         this.inventoryChangeListeners.add(inventoryChangeListener);
@@ -76,7 +77,7 @@ public abstract class LockableContainerBlockEntityMixin implements InventoryChan
     @Override
     public void forwardMajorInventoryChanges(InventoryChangeListener inventoryChangeListener) {
         if (this.inventoryHandlingTypeListeners == null) {
-            this.inventoryHandlingTypeListeners = new ReferenceOpenHashSet<>(1);
+            this.inventoryHandlingTypeListeners = new ReferenceArraySet<>(1);
         }
         this.inventoryHandlingTypeListeners.add(inventoryChangeListener);
     }
