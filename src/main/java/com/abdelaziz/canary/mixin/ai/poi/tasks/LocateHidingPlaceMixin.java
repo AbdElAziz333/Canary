00edfd542ff0ab619f8e96c6df8fa5caf01de611
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-05-23 18:29:46 +0300

    update for 1.19.4

--- src/main/java/com/abdelaziz/canary/mixin/ai/poi/tasks/LocateHidingPlaceMixin.java
+++ src/main/java/com/abdelaziz/canary/mixin/ai/poi/tasks/LocateHidingPlaceMixin.java
@@ -19,14 +19,24 @@ import java.util.function.Predicate;
 public class LocateHidingPlaceMixin {
 
     @Redirect(
-            method = "m_275799_",//lambda$create$5 - m_275799_
+            method = "lambda$create$5",//lambda$create$5 - m_275799_
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/ai/village/poi/PoiManager;getRandom(Ljava/util/function/Predicate;Ljava/util/function/Predicate;Lnet/minecraft/world/entity/ai/village/poi/PoiManager$Occupancy;Lnet/minecraft/core/BlockPos;ILnet/minecraft/util/RandomSource;)Ljava/util/Optional;",
-                    remap = true
+                    target = "Lnet/minecraft/world/entity/ai/village/poi/PoiManager;getRandom(Ljava/util/function/Predicate;Ljava/util/function/Predicate;Lnet/minecraft/world/entity/ai/village/poi/PoiManager$Occupancy;Lnet/minecraft/core/BlockPos;ILnet/minecraft/util/RandomSource;)Ljava/util/Optional;"
             )
     )
     private static Optional<BlockPos> useFasterPOILookup(PoiManager poiManager, Predicate<Holder<PoiType>> typePredicate, Predicate<BlockPos> positionPredicate, PoiManager.Occupancy occupationStatus, BlockPos pos, int radius, RandomSource random) {
         return poiManager.getRandom(new SinglePointOfInterestTypeFilter(POIRegistryEntries.HOME_ENTRY), positionPredicate, occupationStatus, pos, radius, random);
+    }
+
+    @Redirect(
+            method = "lambda$create$8",//lambda$create$8 - m_275799_
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/ai/village/poi/PoiManager;find(Ljava/util/function/Predicate;Ljava/util/function/Predicate;Lnet/minecraft/core/BlockPos;ILnet/minecraft/world/entity/ai/village/poi/PoiManager$Occupancy;)Ljava/util/Optional;"
+            )
+    )
+    private static Optional<BlockPos> useFasterPOILookup(PoiManager pointOfInterestStorage, Predicate<Holder<PoiType>> typePredicate, Predicate<BlockPos> posPredicate, BlockPos pos, int radius, PoiManager.Occupancy occupationStatus) {
+        return pointOfInterestStorage.find(new SinglePointOfInterestTypeFilter(POIRegistryEntries.HOME_ENTRY), posPredicate, pos, radius, occupationStatus);
     }
 }
