f39fd0cf4a50e2db19a32f0c3cd1ee564ee9b453
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-05-24 16:43:59 +0300

    remove some cache properties for now

--- src/main/java/com/abdelaziz/canary/mixin/block/flatten_states/FluidStateMixin.java
+++ src/main/java/com/abdelaziz/canary/mixin/block/flatten_states/FluidStateMixin.java
@@ -2,8 +2,6 @@ package com.abdelaziz.canary.mixin.block.flatten_states;
 
 import com.google.common.collect.ImmutableMap;
 import com.mojang.serialization.MapCodec;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
@@ -14,16 +12,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.Nullable;
-import java.util.stream.Stream;
-
 @Mixin(FluidState.class)
 public abstract class FluidStateMixin {
-    float explosionsResistance;
     boolean isEmpty;
-    boolean isRandomlyTicking;
-    ParticleOptions dripParticles;
-    Stream<TagKey<Fluid>> tags;
 
     @Shadow
     public abstract Fluid getType();
@@ -31,21 +22,7 @@ public abstract class FluidStateMixin {
     @Inject(method = "<init>(Lnet/minecraft/world/level/material/Fluid;Lcom/google/common/collect/ImmutableMap;Lcom/mojang/serialization/MapCodec;)V", at = @At("RETURN"))
     private void initFluidCache(Fluid fluid, ImmutableMap<Property<?>, Comparable<?>> propertyMap,
                                 MapCodec<FluidState> codec, CallbackInfo ci) {
-        explosionsResistance = this.getType().getExplosionResistance();
         isEmpty = this.getType().isEmpty();
-        isRandomlyTicking = this.getType().isRandomlyTicking();
-        dripParticles = this.getType().getDripParticle();
-        tags = this.getType().builtInRegistryHolder().tags();
-    }
-
-    /**
-     * @reason Use cached property
-     * @author AbdElAziz
-     * */
-    @Overwrite
-    @Deprecated
-    public float getExplosionResistance() {
-        return explosionsResistance;
     }
 
     /**
@@ -55,33 +32,5 @@ public abstract class FluidStateMixin {
     @Overwrite
     public boolean isEmpty() {
         return isEmpty;
-    }
-
-    /**
-     * @reason Use cached property
-     * @author AbdElAziz
-     * */
-    @Overwrite
-    public boolean isRandomlyTicking() {
-        return isRandomlyTicking;
-    }
-
-    /**
-     * @reason Use cached property
-     * @author AbdElAziz
-     * */
-    @Nullable
-    @Overwrite
-    public ParticleOptions getDripParticle() {
-        return dripParticles;
-    }
-
-    /**
-     * @reason Use cached property
-     * @author AbdElAziz
-     * */
-    @Overwrite
-    public Stream<TagKey<Fluid>> getTags() {
-        return tags;
     }
 }
