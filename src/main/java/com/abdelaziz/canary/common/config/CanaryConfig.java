903db5d69cf79be675e71a9ec9f6775756ce25ac
AbdElAziz <abdelaziz524aziz@gmail.com>
2023-06-15 16:46:14 +0300

    Fix crash when joining the nether

--- src/main/java/com/abdelaziz/canary/common/config/CanaryConfig.java
+++ src/main/java/com/abdelaziz/canary/common/config/CanaryConfig.java
@@ -32,7 +32,6 @@ public class CanaryConfig {
         this.addMixinRule("ai", true);
         this.addMixinRule("ai.pathing", true);
         this.addMixinRule("ai.poi", true);
-        this.addMixinRule("ai.poi.fast_portals", true);
         this.addMixinRule("ai.poi.tasks", true);
         this.addMixinRule("ai.raid", true);
         this.addMixinRule("ai.sensor", true);
